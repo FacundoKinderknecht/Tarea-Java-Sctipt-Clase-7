24e4fa0ab5305f20afd8566a9bd4cceab8bbbb57
facundo kinderknecht <facukinder@hotmail.com>
2022-05-26 01:55:19 -0300

    Tercer ejercicio

---
+++ Ejercicios JS/Ejercicios/Arrays.js
@@ -0,0 +1,60 @@
+// Arrays
+console.log("")
+console.log("Ejercicios Arrays")
+
+
+
+let array1 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+let array2 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+
+console.log("")
+console.log("Ejercicio 3-a")
+console.log(array1[4], array1[10]);
+
+
+
+console.log("")
+console.log("Ejercicio 3-b")
+console.log(array2.sort())
+
+
+
+array1.push("Meses");
+array1.unshift("Sodiaco");
+console.log("")
+console.log("Ejercicio 3-c")
+console.log(array1)
+
+
+
+array1.shift();
+array1.pop();
+console.log("")
+console.log("Ejercicio 3-d")
+console.log(array1)
+
+
+
+console.log("")
+console.log("Ejercicio 3-e")
+console.log(array1.reverse())
+
+
+
+
+console.log("")
+console.log("Ejercicio 3-f")
+array1.reverse();
+stringNew = array1.join("-");
+console.log(array1.join("-"))
+
+
+
+console.log("")
+console.log("Ejercicio 3.-g")
+console.log(array1.slice(4,11))
+
+console.log("")
+console.log("-------------------------------------------------------------")
