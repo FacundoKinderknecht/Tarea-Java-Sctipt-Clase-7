4c37ae8ceedbe9f0bcdbf20cec18550f21b2ce7f
facundo kinderknecht <facukinder@hotmail.com>
2022-05-26 01:56:22 -0300

    Cuarto ejercicio

---
+++ Ejercicios JS/Ejercicios/IfElse.js
@@ -0,0 +1,32 @@
+
+console.log("");
+console.log("Ejercicios If Else");
+
+
+console.log("");
+console.log("Ejercicio 4-a");
+if (Math.random() > 0.5){
+  console.log("Greater than 0,5")
+}else{
+    console.log("Lower than 0,5")
+}
+
+
+
+console.log("");
+console.log("Ejercicio 4-b");
+let edad = Math.floor(Math.random() * 100 + 1);
+console.log (edad);
+
+if (edad > 65) {
+  console.log("Jubilado");
+} else if (edad < 18) {
+  console.log("Menor");
+} else {
+  console.log("Mayor");
+}
+
+
+
+console.log("");
+console.log("-------------------------------------------------------------");
