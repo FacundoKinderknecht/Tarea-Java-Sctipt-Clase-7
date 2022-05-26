1f417d7ef0c6678b8668278a654e2be9586882a7
facundo kinderknecht <facukinder@hotmail.com>
2022-05-26 01:52:06 -0300

    Primer ejercicio

---
+++ Ejercicios JS/Ejercicios/Variables Y Operadores.js
@@ -0,0 +1,34 @@
+
+console.log("Ejercicios variables y operadores")
+
+let a = "Bienvenidos al";
+
+let b = "cine."
+
+let c = a.concat(" ", b);
+
+
+console.log("")
+console.log("Ejercicio 1-a")
+console.log(c);
+
+
+
+let str1 = "A mi me gustaría";
+
+let str2 = "comer un buen guiso"
+
+let sum = str1.length + str2.length;
+
+
+console.log("")
+console.log("Ejercicio 1-b")
+console.log(sum);
+console.log("")
+console.log("-------------------------------------------------------------")
+
+
+
+
+
+
