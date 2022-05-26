0600024aa8ed928667d130b9e4f47e218694c1ba
facundo kinderknecht <facukinder@hotmail.com>
2022-05-26 01:53:58 -0300

    Segundo ejercicio

---
+++ Ejercicios JS/Ejercicios/Strings.js
@@ -0,0 +1,37 @@
+
+console.log("")
+console.log("Ejercicios Strings")
+
+
+
+
+let string1 = "Ingresar texto indefinido";
+let string2 = string1.substring(0,5);
+
+console.log("")
+console.log("Ejercicio 2-a")
+console.log(string2)
+
+
+
+
+let string4 = "ingresar texto indefinido";
+let string5 = string4.substring(0,1).toUpperCase() + string4.substring(1).toLowerCase();
+
+console.log("")
+console.log("Ejercicio 2-b")
+console.log(string5)
+
+
+
+
+let string6 = string4.indexOf(" ");
+
+console.log("")
+console.log("Ejercicio 2-c")
+console.log(string6)
+console.log("")
+console.log("-------------------------------------------------------------")
+
+
+
